a034633689fe1478f7dede150d568254a0326503
Ramixzx <ramiro.mijalovich@gmail.com>
2022-04-24 19:43:18 -0300

    Se comienza con los estilos basicos de la vista **Details**

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useDebugValue } from 'react';
+import defaultAvatar from '../../Assets/images/avatarDefault.png'
+import './Details.css'
 
 const Details = () => {
   return (
     <>
-      <h1>Que ondis</h1>
+      <main className='container-cardView'>
+        <div className="cardView">
+          <span className='cardView-id'>ID: 4</span>
+          <img className='cardView-image' src={defaultAvatar} alt="" />
+          <h1 className='cardView-title'>Rick</h1>
+          <button className='cardView-button'>Delete</button>
+        </div>
+      </main>
     </>
   );
 }
